90902644bb6261be332f78ba166f05eafd193092
dGarcia-NC <anthonygarcia833@gmail.com>
2022-08-25 10:32:49 -0700

    Add GET request for testing our components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,25 @@
 import React, { useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 
-import RatingsReviews from './components/Reviews/RatingsReviews.jsx';
 import QuestionAnswers from './components/QA/QuestionAnswers.jsx';
+import RatingsReviews from './components/Reviews/RatingsReviews.jsx';
 
 import { ProductContext } from './contexts/product.context.jsx';
 
 const App = () => {
 
   useEffect(() => {
-    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/40344', {
+    var baseURI = process.env.BASE_URI;
+    axios.get(`${baseURI}products/40344`, {
       headers: {
         'Authorization': process.env.GITHUB_TOKEN
       }
     })
       .then(result => {
         console.log('RESULT:', result.data);
-      }
-        )
+      })
   }, [])
+
     return (
       <>
         <QuestionAnswers />
