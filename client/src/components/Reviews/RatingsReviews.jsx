d07cbd9f1fb0d3481e93751bb0853f524e364c91
Daniel Zweig <danielzweig@Daniels-MacBook-Pro.local>
2022-08-25 11:19:01 -0700

    fixed width of reviews and ratings module to be dynamic (at 82%) instead of static, refactored all reviews components to use hooks

--- client/src/components/Reviews/RatingsReviews.jsx
+++ client/src/components/Reviews/RatingsReviews.jsx
@@ -1,29 +1,25 @@
-import React from "react"
+import React, {useState} from "react"
 import ReviewsList from "./ReviewsList.jsx"
 import RatingsBreakdown from "./RatingsBreakdown.jsx"
 import ProductBreakdown from "./ProductBreakdown.jsx"
 import styles from "../../styles/Reviews/reviews.css"
 
-class RatingsReviews extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+const RatingsReviews = () => {
 
-  render() {
-    return (
-      <div className="RR-Container">
-        <div className="rr-title">RATINGS & REVIEWS</div>
-        <div className="Module-Container">
-          <div className="Breakdown-Container">
-            <RatingsBreakdown />
-            <ProductBreakdown />
-          </div>
-          <ReviewsList />
+  return (
+    <div className="RR-Container">
+      <div className="rr-title">RATINGS & REVIEWS</div>
+      <div className="Module-Container">
+        <div className="Breakdown-Container">
+          <RatingsBreakdown />
+          <ProductBreakdown />
         </div>
+        <ReviewsList />
       </div>
-    )
-  }
-
+    </div>
+  )
 }
+
+
 
 export default RatingsReviews
--- client/src/components/Reviews/ReviewTile.jsx
+++ client/src/components/Reviews/ReviewTile.jsx
@@ -1,27 +1,19 @@
 import React from "react"
 
-class ReviewTile extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <div className="ReviewTile-Container">
-        <div>stars</div>
-        <div>username, January 1, 2022</div>
-        <div>summary line</div>
-        <div>
-          this is the body text. lorem ipsum so and so and so blah blah blah blah this is review text
-        </div>
-        <div>I recommend this product</div>
-        <div>Response: I am responding to the review</div>
-        <div>Helpful? Yes(10) | Report</div>
+const = ReviewTile = () => {
+  return (
+    <div className="ReviewTile-Container">
+      <div>stars</div>
+      <div>username, January 1, 2022</div>
+      <div>summary line</div>
+      <div>
+        this is the body text. lorem ipsum so and so and so blah blah blah blah this is review text
       </div>
-    )
-  }
-
-
+      <div>I recommend this product</div>
+      <div>Response: I am responding to the review</div>
+      <div>Helpful? Yes(10) | Report</div>
+    </div>
+  )
 }
 
 
