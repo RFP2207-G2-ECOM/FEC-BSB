1b3e8a180201a24fa5666833ed373f467b476b65
Sean Young <SeanKaicheeYoung@gmail.com>
2022-08-26 11:35:52 -0700

    Included condition rendering of InfoBar in BottomBar

--- client/src/components/Overview/BottomBar/ProductDescription.jsx
+++ client/src/components/Overview/BottomBar/ProductDescription.jsx
@@ -5,18 +5,40 @@ const ProductDescription = () => {
 
   const { product } = useContext(ProductContext);
 
-  return (
-    <div className='Info-Container'>
-      <div className='Slogan-Container'>
-        <div className='Underline'>
+
+  if (product.slogan && product.description){
+    return (
+      <div className='Info-Container'>
+        <div className='Slogan-Container'>
+          <div className='Underline'>
+            <p className='Slogan'>{product.slogan}</p>
+          </div>
+        </div>
+        <div className='Description-Container'>
+          <p className='Description'>{product.description}</p>
+        </div>
+      </div>
+    )
+  } else if (!product.slogan && product.description) {
+    return (
+      <div className='Info-Container'>
+        <div className='Description-Container'>
+          <p className='Description'>{product.description}</p>
+        </div>
+      </div>
+    )
+  } else if (!product.description && product.slogan) {
+    return (
+      <div className='Info-Container'>
+        <div className='Slogan-Container'>
           <p className='Slogan'>{product.slogan}</p>
         </div>
       </div>
-      <div className='Description-Container'>
-        <p className='Description'>{product.description}</p>
-      </div>
-    </div>
-  )
+    )
+  } else {
+    return
+    <div></div>;
+  }
 };
 
 export default ProductDescription;
