7207e8a039af39f4d1685642df2279ef22924e91
Sean Young <SeanKaicheeYoung@gmail.com>
2022-08-26 11:21:04 -0700

    Added BottomBar functionality with slogan, description, and a small piece of aesthetics. Will fix spacing/aesthetics later

--- client/src/components/Overview/BottomBar/ProductDescription.jsx
+++ client/src/components/Overview/BottomBar/ProductDescription.jsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ProductContext } from '../../../contexts/product-info.context.jsx';
 
 const ProductDescription = () => {
+
+  const { product } = useContext(ProductContext);
+
   return (
-    <div className='Description-Container'>
-      <p>Description Placeholder</p>
+    <div className='Info-Container'>
+      <div className='Slogan-Container'>
+        <div className='Underline'>
+          <p className='Slogan'>{product.slogan}</p>
+        </div>
+      </div>
+      <div className='Description-Container'>
+        <p className='Description'>{product.description}</p>
+      </div>
     </div>
   )
 };
