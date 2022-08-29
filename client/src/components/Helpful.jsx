90c9eead481ddf08c2ba8d4eee816312a3f85ff2
dGarcia-NC <anthonygarcia833@gmail.com>
2022-08-29 11:24:51 -0700

    Add whitespace

--- client/src/components/Helpful.jsx
+++ client/src/components/Helpful.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const Helpful = ({helpful, helpfulType, id}) => {
+
   helpfulType = helpfulType || 'answers';
   const [help, setHelp] = useState(helpful);
 
