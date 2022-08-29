e4222b27929d94d5ed781683a917f3d5b7438d98
Sean Young <SeanKaicheeYoung@gmail.com>
2022-08-29 10:43:08 -0700

    Adjusted size of SM Buttons and connected proper icons

--- client/src/components/Overview/BottomBar/SocialMediaButtons.jsx
+++ client/src/components/Overview/BottomBar/SocialMediaButtons.jsx
@@ -3,19 +3,23 @@ import { FaFacebookSquare } from 'react-icons/fa';
 import { FaInstagramSquare } from 'react-icons/fa';
 import { FaTwitterSquare } from 'react-icons/fa';
 import { FaPinterestSquare } from 'react-icons/fa';
+import { IconContext} from 'react-icons';
 
 const SocialMediaButtons = () => {
+  let style = { size:'5em'};
   return (
-    <div className='Social-Media-Buttons'>
-      <div className='SMBRow'>
-        <button className="FB btn"> <FaFacebookSquare /> </button>
-        <button className="IG btn"> <FaInstagramSquare /> </button>
+    <IconContext.Provider value={{className:'btn'}} >
+      <div className='Social-Media-Buttons' >
+        <div className='SMBRow'>
+          <FaFacebookSquare /> {/* Add onClickHandler to go to FaceBook link */}
+          <FaInstagramSquare /> {/* Add onClickHandler to go to Instagram link */}
+        </div>
+        <div className='SMBRow'>
+          <FaTwitterSquare /> {/* Add onClickHandler to go to Twitter link */}
+          <FaPinterestSquare /> {/* Add onClickHandler to go to Pinterest link */}
+        </div>
       </div>
-      <div className='SMBRow'>
-        <button className="TW btn"> <FaTwitterSquare /> </button>
-        <button className="PT btn"> <FaPinterestSquare /> </button>
-      </div>
-    </div>
+    </IconContext.Provider>
   )
 };
 
