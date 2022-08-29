ffd758ad0d9eedac4cbdffd12294c35be2c204b0
Sean Young <SeanKaicheeYoung@gmail.com>
2022-08-29 09:36:49 -0700

    Connected Icons for Social Media Buttons

--- client/src/components/Overview/BottomBar/SocialMediaButtons.jsx
+++ client/src/components/Overview/BottomBar/SocialMediaButtons.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import { FaFacebookSquare } from 'react-icons/fa';
+import { FaInstagramSquare } from 'react-icons/fa';
+import { FaTwitterSquare } from 'react-icons/fa';
+import { FaPinterestSquare } from 'react-icons/fa';
 
 const SocialMediaButtons = () => {
   return (
     <div className='Social-Media-Buttons'>
       <div className='SMBRow'>
-        <button className="FB btn">FB</button>
-        <button className="IG btn">IG</button>
+        <button className="FB btn"> <FaFacebookSquare /> </button>
+        <button className="IG btn"> <FaInstagramSquare /> </button>
       </div>
       <div className='SMBRow'>
-        <button className="TW btn">TW</button>
-        <button className="PT btn">PT</button>
+        <button className="TW btn"> <FaTwitterSquare /> </button>
+        <button className="PT btn"> <FaPinterestSquare /> </button>
       </div>
     </div>
   )
