db7055466fd19b72c22bc19c1387a287fcc9bda8
Sean Young <SeanKaicheeYoung@gmail.com>
2022-08-30 11:37:26 -0700

    Added functionality for SM Buttons, actually links to respective SM Platform
    TODOs:
    - AddToCart functionality
    - Color Scheme for Style Select
    - Product Display & Zoom In Functionality
    - Fix problem with tiny screens on Feature/Material Section

--- client/src/components/Overview/BottomBar/SocialMediaButtons.jsx
+++ client/src/components/Overview/BottomBar/SocialMediaButtons.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { FaFacebookSquare } from 'react-icons/fa';
-import { FaInstagramSquare } from 'react-icons/fa';
-import { FaTwitterSquare } from 'react-icons/fa';
 import { FaPinterestSquare } from 'react-icons/fa';
+import { FaTwitterSquare } from 'react-icons/fa';
+import { FaRedditSquare } from 'react-icons/fa';
 import { IconContext} from 'react-icons';
 
 const SocialMediaButtons = () => {
@@ -13,13 +13,17 @@ const SocialMediaButtons = () => {
           <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=example.org">
           <FaFacebookSquare />
           </a>
-          <FaInstagramSquare />
+          <a target='_blank' href='https://www.pinterest.com/pin/create/button/'>
+            <FaPinterestSquare />
+          </a>
         </div>
         <div className='SMBRow'>
           <a target="_blank" href="https://twitter.com/intent/tweet">
             <FaTwitterSquare />
           </a>
-          <FaPinterestSquare /> {/* Add onClickHandler to go to Pinterest link */}
+          <a target='_blank' href={`https://www.reddit.com/r/test/submit?title=What%20An%20Amazing%20Deal&url=${`Insert Our Own URL Here`}`}>
+            <FaRedditSquare />
+          </a>
         </div>
       </div>
     </IconContext.Provider>
