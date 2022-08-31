42a9a6a00f4d1297e3c127384b448f73d069a6f3
Daniel Zweig <danielzweig@Daniels-MacBook-Pro.local>
2022-08-30 21:29:00 -0700

    added ratingsbreakdownfilter component and imported it into ratingsbreakdown

--- client/src/components/Reviews/RatingsBreakdown.jsx
+++ client/src/components/Reviews/RatingsBreakdown.jsx
@@ -1,29 +1,33 @@
 import React, { useState, useEffect, useContext } from "react"
 import StaticRating from "../StarRating.jsx"
 import { ProductReviewsContext } from "../../contexts/product-reviews.context.jsx"
+import RatingsBreakdownFilter from "./RatingsBreakdownFilter.jsx"
 
 import styles from "../../styles/Reviews/ratingsBreakdown.css"
 
 const RatingsBreakdown = () => {
 
   let { metadata } = useContext(ProductReviewsContext)
-  let ratings = {...metadata.ratings}
+
+  console.log('this is metadata:', metadata)
 
   const [averageRating, setAverageRating] = useState(0)
   const [recommended, setRecommended] = useState(0)
+  const [ratingsObject, setRatingsObject] = useState({})
 
   useEffect(() => {
     if (metadata.ratings) {
-      const totalReviews = Object.values(ratings)
+      const totalReviews = Object.values(metadata.ratings)
         .reduce((a, b) => Number(a) + Number(b), 0)
-      const average = Object.values(ratings)
+      const average = Object.values(metadata.ratings)
         .reduce((a, b, i) =>  Number(a) + Number(b) * (i + 1) ) / totalReviews
       const recommendedRate = metadata.recommended.true / totalReviews
 
       setAverageRating(average)
       setRecommended(recommendedRate)
+      setRatingsObject(metadata.ratings)
     }
-  }, [ratings])
+  }, [metadata])
 
   return (
     <div className="RatingsBreakdown-Container">
@@ -39,6 +43,13 @@ const RatingsBreakdown = () => {
       </div>
       <div className="ratingsBreakdown-recommended">
         {`${Math.round(recommended * 100)}% of reviews recommend this product`}
+      </div>
+      <div className="Ratings-Visual-Container">
+        {[5, 4, 3, 2, 1].map(rating => {
+          <RatingsBreakdownFilter
+            rating={rating}
+          />
+        })}
       </div>
     </div>
   )
