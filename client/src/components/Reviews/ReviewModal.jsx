164a98f626a2c482d95edc81d925d06213fe7c97
Daniel Zweig <danielzweig@Daniels-MacBook-Pro.local>
2022-09-01 00:04:43 -0700

    completed radio button for recommended in review modal

--- client/src/components/Reviews/ReviewModal.jsx
+++ client/src/components/Reviews/ReviewModal.jsx
@@ -2,19 +2,25 @@ import React, { useState, useEffect } from "react"
 import ReactDom from "react-dom"
 
 import ReviewModalStarRating from "./ReviewModalStarRating.jsx"
+import ReviewModalRecommended from "./ReviewModalRecommended.jsx"
 
 import styles from "../../styles/Reviews/reviewModal.css"
 
 const ReviewModal = ({ open, onClose, productName }) => {
 
   const [starRating, setStarRating] = useState(null)
-  const [recommended, setRecommended] = useState(false)
+  const [recommended, setRecommended] = useState(null)
   const [characteristics, setCharacteristics] = useState({})
   const [reviewSummary, setReviewSummary] = useState("")
   const [reviewBody, setReviewBody] = useState("")
   const [photos, setPhotos] = useState([])
   const [nickname, setNickname] = useState("")
   const [email, setEmail] = useState("")
+
+  useEffect(() => {
+
+  console.log('recommended changed!', recommended)
+  },[recommended])
 
   if(!open) return null
 
@@ -27,11 +33,15 @@ const ReviewModal = ({ open, onClose, productName }) => {
         <div>
           <div>Overall rating</div>
           <ReviewModalStarRating
-            key={productName}
             setStarRating={setStarRating}
           />
         </div>
-        <div>Do you recommend this product?</div>
+        <div>
+          <div>Do you recommend this product?</div>
+          <ReviewModalRecommended
+            setRecommended={setRecommended}
+          />
+        </div>
         <div>Charateristics</div>
         <div className="review-modal-summary">
           <div>Review Summary</div>
@@ -56,7 +66,7 @@ const ReviewModal = ({ open, onClose, productName }) => {
         <div>What is your nickname</div>
         <div>Your email</div>
         <button onClick={onClose}>Submit review</button>
-        <button onClick={onClose}>Close Modal</button>
+        <button onClick={onClose}>Cancel</button>
       </div>
     </>,
     document.getElementById('portal')
