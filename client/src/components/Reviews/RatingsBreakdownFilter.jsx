3c9b74464696773a4d9b8f0fe6bb395155f424d4
Daniel Zweig <danielzweig@Daniels-MacBook-Pro.local>
2022-09-02 10:42:46 -0700

    added hovering highlight css to ratings breakdown filters

--- client/src/components/Reviews/RatingsBreakdownFilter.jsx
+++ client/src/components/Reviews/RatingsBreakdownFilter.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react"
 const RatingsBreakdownFilter = ({ rating, totalReviews, ratingsObject, setStarFilters, starFilters }) => {
 
   const [filterStatus, setFilterStatus] = useState(false)
+  const [hovering, setHovering] = useState(false)
 
   const ratings = (ratingsObject[rating] / totalReviews) * 100
   const fillStyle = {width: `${ratings}%`}
@@ -19,13 +20,39 @@ const RatingsBreakdownFilter = ({ rating, totalReviews, ratingsObject, setStarFi
     setFilterStatus(!filterStatus)
   }
 
+  let hoverStyle = {}
+  hovering ? hoverStyle = {...hoverStyle, color: 'gray', cursor: 'pointer'} : hoverStyle = {...hoverStyle}
+
+  let highlightStyle
+  filterStatus ? highlightStyle = {
+    backgroundColor: '#ccc',
+    width: '2.5em',
+    height: '1em',
+    padding: '3px',
+    borderRadius: '5px',
+    position: 'absolute',
+    zIndex: '-1',
+    top: '0',
+    left: '0'
+  } : highlightStyle = {}
+
   return (
     <div className="ratings-breakdown-filter">
-      <div
-        className="ratings-breakdown-filter-button hover"
-        value={rating}
-        onClick={applyStarFilter}
-      >{`${rating} stars`}</div>
+      <div>
+        <div
+          className="ratings-breakdown-filter-button"
+          value={rating}
+          style={hoverStyle}
+          onClick={applyStarFilter}
+          onMouseEnter={() => setHovering(true)}
+          onMouseLeave={() => setHovering(false)}
+        >{`${rating} stars`}
+        </div>
+        <div
+          className='ratings-breakdown-filter-highlight'
+          style={highlightStyle}
+        ></div>
+      </div>
       <div className="ratings-breakdown-filter-bar">
         <div
           className="ratings-breakdown-filter-fill"
