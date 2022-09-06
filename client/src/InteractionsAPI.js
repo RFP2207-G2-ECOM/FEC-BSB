b3510755a32b2f09f4ca5e7897739bd14ca07c7b
dGarcia-NC <anthonygarcia833@gmail.com>
2022-09-06 14:01:37 -0700

    Add Change Requestion Functionality

---
+++ client/src/InteractionsAPI.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const InteractionsAPI = (element, widget) => {
+  const data = {
+    element, // Selector for the element which was clicked
+    widget, // Name of the module/widget in which the click occured
+    time: new Date().toISOString(), // Time the interaction occurred
+  }
+
+  axios.post(`${process.env.BASE_URI}interactions`, data, {
+    headers: {
+      'Authorization': process.env.GITHUB_TOKEN
+    }
+  })
+    .then(response => {
+      console.log('response', response);
+    })
+}
+
+export default InteractionsAPI;
