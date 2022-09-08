51de7987349e0832565a073572c9f2b111250bbf
Sean Young <SeanKaicheeYoung@gmail.com>
2022-09-08 10:31:37 -0700

    Finished all functionality and aesthetics, except for maybe the cart modal pop-up.
    TODOs:
    - Get AWS set up

--- client/src/components/Overview/TopBar/NavBar.jsx
+++ client/src/components/Overview/TopBar/NavBar.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
 
+import { FaSearch, FaUserAlt, FaShoppingCart } from 'react-icons/fa';
+
 const NavBar = () => {
   return (
     <div className='NavBar'>
-      <div className='Logo'>
-        Logo
-      </div>
-      <div className='SearchBar'>
-        <input className='SearchInput' />
-        <button className='SubmitButton'>Submit</button>
+      <div className='innerNavBar'>
+        <div className='TopBarLeft'>
+          <img src={`https://www.officialcharts.com/media/318937/backstreet_boys.jpg?width=796&height=420&mode=stretch`} className='BSBPhoto'></img>
+        </div>
+        <div className='Logo'>
+          BackStreet's Back!
+        </div>
+        <div className='SiteNavBar'>
+          <div className='IconContainer' onClick={()=>{alert('Navigate to Search Page')}} >
+            <FaSearch className='Icon'/>
+          </div>
+          <div className='IconContainer' onClick={()=>{alert('Navigate to User Info Page')}} >
+            <FaUserAlt className='Icon' />
+          </div>
+          <div className='IconContainer' onClick={()=>{alert('Open Cart Modal?')}} >
+            <FaShoppingCart className='Icon'/>
+          </div>
+        </div>
       </div>
     </div>
   )
